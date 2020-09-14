d12d49d13b4ffc3707a6b9365a85e5b3a807644a
Samuel Hyeman <Hyeman1738@gmail.com>
2020-09-14 22:39:25 +0100

    Image Feature

--- models/Question.js
+++ models/Question.js
@@ -3,13 +3,14 @@ const Mongoose=require('mongoose');
 const OptionSchema = new Mongoose.Schema({
     "Title":{type:String,required:true},
     "IsCorrect":{type:Boolean,default:false},
-    "Index":{type:Number,required:true}
+    "Index":{type:Number,required:true},   
 })
 
 const QuestionSchema=new Mongoose.Schema({
     "Title":{type:String,required:true},
     "Options":[{type:OptionSchema}],
-    "CorrectOptionIndex":{type:Number,required:true}
+    "CorrectOptionIndex":{type:Number,required:true},
+    "ImageUrl":{type:String,default:null}
 })
     // QuestionSchema.pre("deleteOne",function(next){
     //     this.model("Course").deleteOne({"Questions":this._id},next)
